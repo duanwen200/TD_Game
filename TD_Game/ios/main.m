150e3f1d4112997f973dfaac7a40f8d3fae82199
shilong <longlingxiu@gmail.com>
2013-11-11 13:45:20 +0800

    pushing code

---
+++ TD_Game/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TD_Game
+//
+//  Created by cesc on 13-11-11.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
